263c3a0e28661f6406ca7cd6962b03dca2a480ba
priyanshusaraf <66620510+priyanshusaraf@users.noreply.github.com>
2020-06-18 21:00:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//providing buttons with href
+document.getElementById("myButton1").onclick = function (){
+    location.href="file:///C:/Users/USER/Desktop/my_Website/cool_website/corona_tracer/google%20maps/index.html";
+};
+document.getElementById('mybutton').onclick = function(){
+                location.href = "official-form-community.html";
+}
+document.getElementById('button').onclick = function (){
+    location.href= "form-for-donarions.html" ;
+};             
